9687c04502d1921d54d6ecdb499711616ed80ce8
Yashas HK <yashas2k5@gmail.com>
2026-09-22 03:06:49 +0530

    Fix seed_demo_users: add required age, district, license_number fields

--- core/management/commands/seed_demo_users.py
+++ core/management/commands/seed_demo_users.py
@@ -10,7 +10,13 @@ class Command(BaseCommand):
         # Patient - ravi
         if not User.objects.filter(username='ravi').exists():
             u = User.objects.create_user('ravi', 'ravi@gmail.com', 'ravi123')
-            PatientProfile.objects.create(user=u, phone_number='9876543211', aadhaar_last4='1234')
+            PatientProfile.objects.create(
+                user=u, age=28, district='Bengaluru Urban',
+                economic_status='BPL', has_ration_card=True,
+                has_aadhaar=True, aadhaar_last4='1234',
+                disease_type='None',
+                phone_number='9876543211'
+            )
             self.stdout.write(self.style.SUCCESS('Created patient: ravi'))
         else:
             self.stdout.write('Patient ravi already exists')
@@ -18,7 +24,7 @@ class Command(BaseCommand):
         # Hospital Staff - Parinitha
         if not User.objects.filter(username='Parinitha').exists():
             u2 = User.objects.create_user('Parinitha', 'parinitha@gmail.com', 'parinitha123')
-            HospitalStaff.objects.create(user=u2, staff_name='Parinitha', hospital_name='SDM', is_verified=True)
+            HospitalStaff.objects.create(user=u2, staff_name='Parinitha', hospital_name='SDM', license_number='SDM001', is_verified=True)
             self.stdout.write(self.style.SUCCESS('Created staff: Parinitha'))
         else:
             self.stdout.write('Staff Parinitha already exists')
