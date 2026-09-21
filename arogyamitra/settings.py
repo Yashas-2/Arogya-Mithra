c503ebc57eec0ddf736888ea2a04ee1a4e3822dd
Yashas HK <yashas2k5@gmail.com>
2026-09-21 22:03:45 +0530

    Production-ready: env vars, whitenoise storage, render.yaml

--- arogyamitra/settings.py
+++ arogyamitra/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#asstm)y#r-l@qen)gb7d&6xg0et7yromsc*_7-uii+rsqkk*7'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-#asstm)y#r-l@qen)gb7d&6xg0et7yromsc*_7-uii+rsqkk*7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default='True', cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
@@ -131,6 +131,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
